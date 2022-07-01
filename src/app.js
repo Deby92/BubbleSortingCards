2f83953f96f96e3040acaf9f810be8e6e80cc8b4
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-07-01 14:07:32 +0000

    viernes

--- src/app.js
+++ src/app.js
@@ -9,3 +9,20 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+/* const bubbleSort = arr => {
+  let wall = arr.length - 1; //we start the wall at the end of the array
+  while (wall > 0) {
+    let index = 0;
+    while (index < wall) {
+      //compare the adjacent positions, if the right one is bigger, we have to swap
+      if (arr[index] > arr[index + 1]) {
+        let aux = arr[index];
+        arr[index] = arr[index + 1];
+        arr[index + 1] = aux;
+      }
+      index++;
+    }
+    wall--; //decrease the wall for optimization
+  }
+  return arr;
+}; */
